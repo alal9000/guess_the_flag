0b7e1762a0528c612b77c71c8312e21fb10abea5
Aaron <aaronlal61@gmail.comgit config --global user.email aaronlal61@gmail.com>
2025-06-20 05:48:38 +1000

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Aaron Lal on 8/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
